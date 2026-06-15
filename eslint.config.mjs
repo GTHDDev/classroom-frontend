bcf22311324ceddbf4d1c446081fba1b571f95d4
Miguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2026-06-14 22:02:44 -0600

    chore: initial commit with Refine architecture setup

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierConfig from 'eslint-config-prettier'
+
+const eslintConfig = defineConfig([
+	...nextVitals,
+	...nextTs,
+	{
+		rules: {
+			'no-unexpected-multiline': 'error',
+			'react/no-unknown-property': [
+				'error',
+				{
+					ignore: [
+						'position',
+						'rotation',
+						'scale',
+						'args',
+						'intensity',
+						'attach',
+						'geometry',
+						'material',
+						'castShadow',
+						'receiveShadow',
+						'dispose',
+						'angle',
+						'decay',
+						'penumbra',
+						'position',
+						'rotation',
+						'scale',
+						'args',
+						'intensity',
+						'attach',
+						'geometry',
+						'material',
+						'castShadow',
+						'receiveShadow',
+						'map',
+						'toneMapped'
+					]
+				}
+			],
+			'jsx-a11y/alt-text': 'error',
+			'jsx-a11y/aria-props': 'error',
+			'jsx-a11y/aria-proptypes': 'error',
+			'jsx-a11y/role-has-required-aria-props': 'error'
+		}
+	},
+	prettierConfig,
+	globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])
+])
+
+export default eslintConfig
--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,62 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { GitHubBanner, Refine } from '@refinedev/core'
+import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools'
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
 
 import routerProvider, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
-import "./App.css";
-import { Toaster } from "./components/refine-ui/notification/toaster";
-import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
-import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import { dataProvider } from "./providers/data";
+	DocumentTitleHandler,
+	UnsavedChangesNotifier
+} from '@refinedev/react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import './App.css'
+import { Toaster } from './components/refine-ui/notification/toaster'
+import { useNotificationProvider } from './components/refine-ui/notification/use-notification-provider'
+import { ThemeProvider } from './components/refine-ui/theme/theme-provider'
+import { dataProvider } from './providers/data'
+import Dashboard from './pages/dashboard'
+import { Home } from 'lucide-react'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ThemeProvider>
-          <DevtoolsProvider>
-            <Refine
-              dataProvider={dataProvider}
-              notificationProvider={useNotificationProvider()}
-              routerProvider={routerProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                projectId: "AIJwJa-6HALX1-eNJXj3",
-              }}
-            >
-              <Routes>
-                <Route index element={<WelcomePage />} />
-              </Routes>
-              <Toaster />
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-            <DevtoolsPanel />
-          </DevtoolsProvider>
-        </ThemeProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<GitHubBanner />
+			<RefineKbarProvider>
+				<ThemeProvider>
+					<DevtoolsProvider>
+						<Refine
+							dataProvider={dataProvider}
+							notificationProvider={useNotificationProvider()}
+							routerProvider={routerProvider}
+							options={{
+								syncWithLocation: true,
+								warnWhenUnsavedChanges: true,
+								projectId: 'AIJwJa-6HALX1-eNJXj3'
+							}}
+							resources={[
+								{
+									name: 'dashboard',
+									list: '/',
+									meta: {
+										label: 'Home',
+										icon: <Home />
+									}
+								}
+							]}
+						>
+							<Routes>
+								<Route index element={<Dashboard />} />
+							</Routes>
+							<Toaster />
+							<RefineKbar />
+							<UnsavedChangesNotifier />
+							<DocumentTitleHandler />
+						</Refine>
+						<DevtoolsPanel />
+					</DevtoolsProvider>
+				</ThemeProvider>
+			</RefineKbarProvider>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
+// export default App
